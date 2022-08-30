753f216bb1f5b4c13bbd67a8cbef05010f6d671f
Daniel Chun <danielchun31@gmail.com>
2022-08-30 14:10:21 -0700

    Function puts titles into a new array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,16 @@
-const getTheTitles = function() {
+const getTheTitles = (function(x) {
+    let newArray = [];
+    newArray.push(x[0].title);
+    newArray.push(x[1].title);
+    return newArray;
+});
 
-};
+//There is an array that contains two objects (each with a title and author).
+//Put each object through a function that extracts the titles and puts them into
+//a new array.
+
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
