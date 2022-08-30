2dbaf7cbde1526bdfc7ab6705096fc78299d9adc
Daniel Chun <danielchun31@gmail.com>
2022-08-30 09:18:04 -0700

    Fibonacci function works for positive numbers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    const seqArray = [1,1];
 
+    for (i = 0; i < x; i++) {
+        let newValue = (seqArray[seqArray.length - 1]) + (seqArray[seqArray.length - 2]);
+        seqArray.push(newValue);
+    }
+
+    return parseInt(seqArray[x - 1]);
 };
+
+//
 
 // Do not edit below this line
 module.exports = fibonacci;
