79ffcec0ab18c27c2597354a882c833e16d0ed55
Daniel Chun <danielchun31@gmail.com>
2022-09-02 11:42:08 -0700

    Function now passes all three tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,29 +1,31 @@
 const findTheOldest = function(array) {
   
     const years = array.map(function (obj) {   
-        if ("yearOfDeath" in obj) {
-            return (obj.yearOfDeath - obj.yearOfBirth); 
-        } else {
-            const date = new Date();
-            obj.yearOfDeath = date.getFullYear();
-        }
+      if ("yearOfDeath" in obj) {
+          return (obj.yearOfDeath - obj.yearOfBirth); 
+      } else {
+          const date = new Date();
+          obj.yearOfDeath = date.getFullYear();
+          return (obj.yearOfDeath - obj.yearOfBirth);     
+      }
     }); 
   
     const calcAge = array.map(function (obj) {
-        return (obj.yearOfDeath - obj.yearOfBirth);     
+      return (obj.yearOfDeath - obj.yearOfBirth);     
     });
-
-    console.log(calcAge);
   
-    const compare = function (calcAge) {
-      if (calcAge[0] > calcAge[1] && calcAge[0] > calcAge[2]) {
+    const compare = function (x) {
+      if (x[0] > x[1] && x[0] > x[2]) {
         return array[0];
-      } else if (calcAge[1] > calcAge[0] && calcAge[1] > calcAge[2]) {
+      } else if (x[1] > x[0] && x[1] > x[2]) {
         return array[1];
-      } else if (calcAge[2] > calcAge[0] && calcAge[2] > calcAge[1]) {
+      } else if (x[2] > x[0] && x[2] > x[1]) {
         return array[2];
       }
-   };
+    };
+  
+    return (compare(calcAge));
+
 };
 
 // Do not edit below this line
