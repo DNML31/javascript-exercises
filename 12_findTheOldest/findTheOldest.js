e2cdde74a7a7f67e1317167976e523bda2ce787d
Daniel Chun <danielchun31@gmail.com>
2022-09-02 11:25:56 -0700

    Function returns the ages of objects but the first test no longer works

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  
+    const years = array.map(function (obj) {   
+        if ("yearOfDeath" in obj) {
+            return (obj.yearOfDeath - obj.yearOfBirth); 
+        } else {
+            const date = new Date();
+            obj.yearOfDeath = date.getFullYear();
+        }
+    }); 
+  
+    const calcAge = array.map(function (obj) {
+        return (obj.yearOfDeath - obj.yearOfBirth);     
+    });
 
+    console.log(calcAge);
+  
+    const compare = function (calcAge) {
+      if (calcAge[0] > calcAge[1] && calcAge[0] > calcAge[2]) {
+        return array[0];
+      } else if (calcAge[1] > calcAge[0] && calcAge[1] > calcAge[2]) {
+        return array[1];
+      } else if (calcAge[2] > calcAge[0] && calcAge[2] > calcAge[1]) {
+        return array[2];
+      }
+   };
 };
 
 // Do not edit below this line
